45886c085a8e7a527b33ba2fcd14b79331ec12e8
Lennon hitzeman <hitzemanl@yahoo.com>
2021-01-10 14:50:00 -0800

    modified yesterdays code so that it is easier to use

--- binaryToNumber.py
+++ binaryToNumber.py
@@ -1,17 +1,21 @@
-userInput = input('Binary Number: ')
-validText = True
-for char in userInput:
-    if not char.isnumeric():
-        validText = False
-        break
-    else:
-        intText = (int) (char)
-        if not (intText == 0 or intText == 1):
+def binaryIntoInt (number):
+    validText = True
+    for char in userInput:
+        if not char.isnumeric():
             validText = False
             break
-if validText:
-    print(str(int(userInput,2)))
-else:
-    print('This is not a valid binary number. please restart the program and try again')
+        else:
+            intText = (int) (char)
+            if not (intText == 0 or intText == 1):
+                validText = False
+                break
+    if validText:
+        number = int(userInput,2)
+        print(str(number))
+        return number
+    else:
+        print('This is not a valid binary number. please restart the program and try again')
+        return None
 
-
+userInput = input('Binary Number: ')
+binaryIntoInt(userInput)

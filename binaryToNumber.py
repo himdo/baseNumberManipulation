bc2d3b7bcbf3c1dd4c207e5ed5792380e9fd2c2f
Lennon hitzeman <hitzemanl@yahoo.com>
2021-01-10 18:08:56 -0800

    added convert int to binary function

--- binaryToNumber.py
+++ binaryToNumber.py
@@ -17,5 +17,26 @@ def binaryIntoInt (number):
         print('This is not a valid binary number. please restart the program and try again')
         return None
 
-userInput = input('Binary Number: ')
-binaryIntoInt(userInput)
+def intIntoBinary (number):
+    validText = True
+    for char in userInput:
+        if not char.isnumeric():
+            validText = False
+            break
+    if validText:
+        number = "{0:b}".format(int(number))
+        print(str(number))
+        return number
+    else:
+        print('This is not a valid number. please restart the program and try again')
+        return None
+
+
+userInput = input('Number: ')
+base = input('Base: ')
+if base == '2':
+    binaryIntoInt(userInput)
+elif base == '10':
+    intIntoBinary(userInput)
+else:
+    print('Incorrect setting')
